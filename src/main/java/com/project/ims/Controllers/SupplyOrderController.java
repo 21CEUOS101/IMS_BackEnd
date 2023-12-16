439b5df5f135f27e2da7f956827f8d3c8a20092d
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-12-16 19:30:18 +0530

    imported necessary services in controllers

--- src/main/java/com/project/ims/Controllers/SupplyOrderController.java
+++ src/main/java/com/project/ims/Controllers/SupplyOrderController.java
@@ -1,10 +1,60 @@
 package com.project.ims.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.ims.IServices.IAdminService;
+import com.project.ims.IServices.ICustomerService;
+import com.project.ims.IServices.IDeliveryManService;
+import com.project.ims.IServices.IOrderService;
+import com.project.ims.IServices.IProductService;
+import com.project.ims.IServices.IRSOService;
+import com.project.ims.IServices.IReturnOrderService;
+import com.project.ims.IServices.ISupplierService;
+import com.project.ims.IServices.ISupplyOrderService;
+import com.project.ims.IServices.IW2WOrderService;
+import com.project.ims.IServices.IWManagerService;
+import com.project.ims.IServices.IWareHouseService;
 
 @RestController
 @RequestMapping("/api")
 public class SupplyOrderController {
+    // Autowiring all the services
     
+    @Autowired
+    private IAdminService adminService;
+
+    @Autowired
+    private ICustomerService customerService;
+
+    @Autowired
+    private IDeliveryManService deliveryManService;
+
+    @Autowired
+    private IOrderService orderService;
+
+    @Autowired
+    private IProductService productService;
+
+    @Autowired
+    private ISupplierService supplierService;
+
+    @Autowired
+    private IWareHouseService wareHouseService;
+
+    @Autowired
+    private IWManagerService wManagerService;
+
+    @Autowired
+    private IReturnOrderService returnOrderService;
+
+    @Autowired
+    private ISupplyOrderService supplyOrderService;
+
+    @Autowired
+    private IW2WOrderService w2wOrderService;
+
+    @Autowired
+    private IRSOService returnSupplyOrderService;   
 }
